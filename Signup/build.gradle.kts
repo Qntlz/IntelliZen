0a410d95d8e66f5e9467dd8ab0bdaff960ceea51
Alias314 <83777973+Alias314@users.noreply.github.com>
2025-05-14 16:53:13 +0800

    integrated login/signup

--- Signup/build.gradle.kts
+++ Signup/build.gradle.kts
@@ -1,19 +1,16 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
 }
 
 android {
-    namespace = "com.cerenio.mindvault"
+    namespace = "com.cerenio.signup"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.cerenio.mindvault"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -32,15 +29,11 @@ android {
 }
 
 dependencies {
-    implementation(project(":Flashcards"))
-    implementation(project(":Notes"))
-    implementation(project(":Pomodoro"))
-    implementation(project(":Planner"))
-    implementation(project(":Signup"))
+
     implementation(libs.appcompat)
     implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
